ef933fa69b448ada4972af23d20667dd6be8657a
alasmaritza <alas.maritza.j@gmail.com>
2023-02-24 19:36:47 -0800

    tinkering

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World!</Text>
+      <Image
+        source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
+        style={styles.image}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#73e6cd',
+    fontWeight: 'bold',
+    fontSize: 60,
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
